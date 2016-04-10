9d8d171651913950635902a1ffa6e23015f85158
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2016-04-10 12:37:02 +0530

    Completed Chapter 2 Angular_2_Jumpstart

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
 import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component'
+import {AuthorComponent} from './author.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>My First Angular 2 App</h1>
+               <courses></courses>
+               <authors></authors>`,
+    directives: [CoursesComponent, AuthorComponent]
 })
 export class AppComponent { }
